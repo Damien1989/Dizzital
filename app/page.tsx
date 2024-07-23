32142451a928dca14feda72855dc4cdedfc9057e
damien <contact.damiendiaz@gmail.com>
2024-07-23 17:25:11 +0200

    menu created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Navbar from './_components/Navbar';
+import Menu from './_components/Menu';
+
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Menu />
     </main>
   );
 }
